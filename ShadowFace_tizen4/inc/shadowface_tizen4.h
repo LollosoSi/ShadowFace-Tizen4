71c6b86da0b7dc06d1d8595fe2f4d201ba13c00c
LollosoSi <lollosositv@gmail.com>
2020-04-13 20:16:19 +0200

    Source upload

---
+++ ShadowFace_tizen4/inc/shadowface_tizen4.h
@@ -0,0 +1,18 @@
+#ifndef __shadowface_tizen4_H__
+#define __shadowface_tizen4_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "shadowface_tizen4"
+
+#if !defined(PACKAGE)
+#define PACKAGE "ciabbale.shadowface"
+#endif
+
+#endif /* __shadowface_tizen4_H__ */
